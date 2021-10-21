0c24ccaa5f5b80d544bd613fdf4726718a0a6f9a
Sujit S. Molleti <ssm189@ilab4.cs.rutgers.edu>
2021-10-21 01:51:07 -0400

    added loop

--- shell.c
+++ shell.c
@@ -1,7 +1,25 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int main (int argc, char **argv){
+	
+	char* input = NULL;
+	size_t n = 0;
+
+	printf("> ");
+	while(getline(&input, &n, stdin) > 0 ){
+		printf("%s", input);
+		printf("> ");
+
+//		Uncomment to test for clean input
+//		if(strcmp(input, "exit") == 0){
+//			break;
+//		}
+
+		input = NULL;
+	}
+
 	return EXIT_SUCCESS;
 }
