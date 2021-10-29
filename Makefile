5082a13a8a4f51a1db1016980306d9affb751b81
Sujit S. Molleti <ssm189@ilab2.cs.rutgers.edu>
2021-10-29 00:31:25 -0400

    compiled childprocess input shell

--- Makefile
+++ Makefile
@@ -2,6 +2,14 @@ CFLAGS = -Wall -g -fsanitize=address
 
 %: %.c
 	gcc $(CFLAGS) -o $@ $^
+
+all:
+	gcc -Wall -Werror -c -o childprocess.o childprocess.c
+	gcc -Wall -Werror -c -o input.o input.c
+	gcc -Wall -Werror -c -o shell.o shell.c
+	gcc -Wall -Werror -o shell shell.o input.o childprocess.o
+
 clean:
 	rm -rf shell
 	rm -rf child
+
--- constants.h
+++ constants.h
@@ -1,9 +1,11 @@
 #include <stdlib.h>
+
 #define TRUE 1
 #define FALSE 0
 
-char WHITESPACE_DELIMITER [3] = " \n";
-char* AMPERSAND = "&";
-char BACKGROUND_PATTERN [9]= "^.*&\\s*$";
-char PATH [5] = "PATH";
-char PATH_VAR [14] = "/bin:/usr/bin";
+
+static char WHITESPACE_DELIMITER [3] = " \n";
+static char* AMPERSAND = "&";
+static char BACKGROUND_PATTERN [9]= "^.*&\\s*$";
+static char PATH [5] = "PATH";
+static char PATH_VAR [14] = "/bin:/usr/bin";
