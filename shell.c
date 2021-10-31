0abca0b18f93940df5dfc422d62940158c0b7c9d
Sujit S. Molleti <ssm189@ilab4.cs.rutgers.edu>
2021-10-31 14:12:22 -0400

    jobs and background jobs working

--- shell.c
+++ shell.c
@@ -34,45 +34,48 @@ int main(int argc, char **argv)
 		printf("%s\n", input);
 		//Returns TRUE: 1 or FALSE: 0
 		int isBackground = runInBackground(input);
-		printf("%d\n", isBackground);
 
 		char **args = splitString(input, isBackground); /* block signals */
 
 		int isIC = isInternalCommand(args, jobs);
-
 		/*
 			input checking  
 		*/
 
 		// if it is not a built in command
-		pid_t pid = fork();
-		sigprocmask(SIG_BLOCK, &maskOne, &prevOne);
-		if (pid == 0)
-		{
-			sigprocmask(SIG_SETMASK, &prevOne, NULL);
-			executeChild(args[0], args);
-		}
-		else
-		{
-			sigprocmask(SIG_BLOCK, &maskAll, &prevAll);
-			pid_t processID = getpid();
-			// set group id to process ID
-			setpgid(processID, processID);
-			Child *child = (Child *)malloc(sizeof(Child)); 
-			populateChild(child, args, processID, processID, isBackground, input);
-			addJob(&jobs, child);
-			printAllJobs(&jobs);
-			sigprocmask(SIG_SETMASK, &prevAll, NULL);
-
-			if (!isBackground)
+		if(!isIC){
+			pid_t pid = fork();
+			sigprocmask(SIG_BLOCK, &maskOne, &prevOne);
+			if (pid == 0)
 			{
-				int status;
-				printf("waiting for child to complete yooo\n");
-				waitpid(pid, &status, 0);
+				sigprocmask(SIG_SETMASK, &prevOne, NULL);
+				executeChild(args[0], args);
+			}
+			else
+			{
+				sigprocmask(SIG_BLOCK, &maskAll, &prevAll);
+				pid_t processID = getpid();
+				// set group id to process ID
+				setpgid(processID, processID);
+				Child *child = (Child *)malloc(sizeof(Child)); 
+				populateChild(child, args, processID, processID, isBackground, input);
+				addJob(&jobs, child);
+				// printAllJobs(&jobs);
+				sigprocmask(SIG_SETMASK, &prevAll, NULL);
+
+				if (!isBackground)
+				{
+					int status;
+					printf("waiting for child to complete yooo\n");
+					waitpid(pid, &status, 0);
+				}
 			}
 		}
 
 		printf("> ");
+
+		input = NULL;
+		n = 0;
 	}
 
 	return EXIT_SUCCESS;
